e0feb71456243c5762cef031981480bbee3febd4
Coco <keyaoxia08@gmail.com>
2025-06-23 23:57:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,41 @@ document.addEventListener("DOMContentLoaded", function () {
         prompt.style.display = 'none';
         message.style.display = 'block';
 
+        //SPARKLESSS
+         tsParticles.load("tsparticles", {
+            fullScreen: {
+                enable: false
+            },
+            particles: {
+                number: {
+                    value: 100,
+                    density: { enable: true, value_area: 800 }
+                },
+                color: {
+                    value: "#ffffff"
+                },
+                shape: {
+                    type: "star"
+                },
+                opacity: {
+                    value: 0.8
+                },
+                size: {
+                    value: 3,
+                    random: true
+                },
+                move: {
+                    enable: true,
+                    speed: 1,
+                    direction: "none",
+                    outModes: "out"
+                }
+            },
+            background: {
+                color: "transparent"
+            }
+        });
+
         setTimeout(() => {
             window.location.href = "https://www.canva.com/design/DAGrJQReGlg/GplZ0c8PmM0EOy0R31skCQ/view?utm_content=DAGrJQReGlg&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hcca8658c5a";
         }, 2000); // wait 2 seconds before redirecting
