16428d4f62d3e8c1e2d6ffcfe1256d23d46036c0
Coco <keyaoxia08@gmail.com>
2025-06-23 23:47:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //script.js file
 document.addEventListener("DOMContentLoaded", function () {
-        const btn = document.getElementById('surpriseBtn');
-        const message = document.getElementById('hiddenMessage');
-        const prompt = document.getElementById('prompt');
-        
-        btn.addEventListener('click', () => {
-            btn.style.display = 'none';
-            prompt.style.display = 'none';
-            message.style.display = 'block';
-        });    
+    const btn = document.getElementById('surpriseBtn');
+    const message = document.getElementById('hiddenMessage');
+    const prompt = document.getElementById('prompt');
+
+    btn.addEventListener('click', () => {
+        btn.style.display = 'none';
+        prompt.style.display = 'none';
+        message.style.display = 'block';
+
+        setTimeout(() => {
+            window.location.href = "https://www.canva.com/design/DAGrJQReGlg/GplZ0c8PmM0EOy0R31skCQ/view?utm_content=DAGrJQReGlg&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hcca8658c5a";
+        }, 2000); // wait 2 seconds before redirecting
+    });
 });
