b52e7f3c82e3b54f4f12c302db7a718d02c42574
Coco <keyaoxia08@gmail.com>
2025-06-24 00:00:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
         prompt.style.display = 'none';
         message.style.display = 'block';
 
-        setTimeout(() => {
-            window.location.href = "https://www.canva.com/design/DAGrJQReGlg/GplZ0c8PmM0EOy0R31skCQ/view?utm_content=DAGrJQReGlg&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hcca8658c5a";
-        }, 1000000000000); // wait for a long time before redirecting
+        // setTimeout(() => {
+        //     window.location.href = "https://www.canva.com/design/DAGrJQReGlg/GplZ0c8PmM0EOy0R31skCQ/view?utm_content=DAGrJQReGlg&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hcca8658c5a";
+        // }, 1000000000000); // wait for a long time before redirecting
     });
 });
