90250b0ece041e9410b902b0e4f5a36a496ab043
Coco <keyaoxia08@gmail.com>
2025-06-24 19:40:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
         prompt.style.display = 'none';
         message.style.display = 'block';
 
-        // Play music on button click
-        music.play();
-
         confetti({
             particleCount: 150,
             spread: 70,
             origin: { y: 0.6 }
         });
+        
+        // Play music on button click
+        music.play();
     });
 });
